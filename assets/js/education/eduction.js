4f04f59b46ca58a8d007a5b14e9faa796bf03d9b
karim <karimboulad23@gmail.com>
2024-07-05 19:17:57 +0300

    handle side bar  active link - handle add education  and dashbaord

--- assets/js/education/eduction.js
+++ assets/js/education/eduction.js
@@ -7,6 +7,9 @@ $(document).ready(function(){
     $('.close, .close-footer').click(function(){
       $('#exampleModalCenter').modal('hide');
     });
+    $('#close-footers').click(function(){
+      $('#exampleModalCenter').modal('hide');
+    });
 
     $('.container-img').click(function(){
       $('#exampleModalCenter').modal('hide');
@@ -84,3 +87,108 @@ function getVideoSrc1(param){
   $("#upload1 span").append(`<span>${img_name}</span>`)
   };
 
+
+
+  $(document).ready(function() {
+    // Initialize the datepicker
+
+
+    // Show datepicker on button click
+    $('.input-group-btn .date-set').click(function() {
+      $(this).closest('.input-group').find('input').focus();
+    });
+  });
+
+
+
+  $(document).ready(function() {
+    var pathname = window.location.pathname;
+    // Get the links elements
+    var Educations_link = $('#Educations-link');
+    var admin_link = $('#admin-link');
+    var dashboard_link = $('#dashboard-link');
+    var banner_link = $('#banner-link');
+    var area_link = $('#area-link');
+    var page_link = $('#page-link');
+    var contact_link = $('#contact-link');
+    var oper_link = $('#oper-link');
+    var oper_link = $('#oper-link');
+    var oper_link = $('#oper-link');
+    var setting_link = $('#setting-link');
+    var role_link = $('#role-link');
+    var promo_link = $('#promo-link');
+    var report_link = $('#report-link');
+    var user_link = $('#user-link');
+    var paymentSetting_link = $('#paymentSetting-link');
+    var sendn_link = $('#sendn-link');
+    // Check the path and set the active link accordingly
+    if (pathname.includes('/education/') ) {
+        Educations_link.addClass('active-link');
+    } else if (
+        pathname.includes('/admins/index') 
+ 
+    ) {
+      dashboard_link.addClass('active-link');
+    } else if (
+        pathname.includes('/admin/block') ||
+        pathname.includes('/admin/terms') ||
+        pathname.includes('/admin/contactus') ||
+        pathname.includes('/admin/system-banner') ||
+        pathname.includes('/admin/accessibility-information') ||
+        pathname.includes('/admin/privacy-policy') ||
+        pathname.includes('/admin/block-all')
+    ) {
+        block_link.addClass('active-link');
+    } 
+
+
+
+    else if (pathname.includes('/admin/users')) {
+        user_link.addClass('active-link');
+    }
+    else if (pathname.includes('/admin/sendNotif')) {
+        sendn_link.addClass('active-link');
+    }
+    else if (pathname.includes('/payment-method-settings')) {
+        paymentSetting_link.addClass('active-link');
+    }
+    else if (pathname.includes('/admin/reports')) {
+        report_link.addClass('active-link');
+    }
+    else if (pathname.includes('/admin/promoCodes')) {
+        promo_link.addClass('active-link');
+    }
+    else if (pathname.includes('/admin/role')) {
+        role_link.addClass('active-link');
+    }
+    else if (pathname.includes('/admin/setting')) {
+        setting_link.addClass('active-link');
+    }
+    else if (pathname.includes('/admin/oper')) {
+        oper_link.addClass('active-link');
+    }
+    else if (pathname.includes('/admin/oper')) {
+        oper_link.addClass('active-link');
+    }
+    else if (pathname.includes('/admin/oper')) {
+        oper_link.addClass('active-link');
+    }
+
+
+     else if (pathname.includes('/admin/contact')) {
+        contact_link.addClass('active-link');
+    } else if (pathname.includes('/admin/pages')) {
+        page_link.addClass('active-link');
+    } else if (pathname.includes('/admin/banners')) {
+        banner_link.addClass('active-link');
+    } else if (pathname.includes('/admin/admins') || pathname.includes('/admin/admins')) {
+        admin_link.addClass('active-link');
+    } else if (pathname.includes('/admin/areas')) {
+        area_link.addClass('active-link');
+    }
+});
+
+
+
+
+// 
